031408c024b8095869ff5a24b613f27713a83e9c
joão pedro cruz <joaocruzzdev@gmail.com>
2025-05-09 15:35:23 -0300

    criando filtros do pedido e separando componentes

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,54 @@
+import { Search, ArrowRight, X } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { TableCell, TableRow } from "@/components/ui/table";
+
+export function OrderTableRow() {
+   return (
+      <TableRow>
+         <TableCell>
+            <Button variant={"outline"} size={"xs"}>
+               <Search className="max-h-3 max-w-3" />
+               <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+         </TableCell>
+
+         <TableCell className="font-mono text-xs font-medium">
+            395t347596238
+         </TableCell>
+
+         <TableCell className="text-muted-foreground">
+            Há 15 minutos
+         </TableCell>
+
+         <TableCell>
+            <div className="flex items-center gap-2">
+               <span className="h-2 w-2 rounded-full bg-slate-400" />
+               <span className="font-medium text-muted-foreground">Pendente</span>
+            </div>
+         </TableCell>
+
+         <TableCell className="font-medium">
+            João Pedro Cruz
+         </TableCell>
+
+         <TableCell className="font-medium">
+            R$ 149,90
+         </TableCell>
+
+         <TableCell>
+            <Button variant={"outline"} size={"xs"}>
+               <ArrowRight className="h-3 w-3" />
+               Aprovar
+            </Button>
+         </TableCell>
+
+         <TableCell>
+            <Button variant={"ghost"} size={"xs"}>
+               <X className="h-3 w-3" />
+               Cancelar
+            </Button>
+         </TableCell>
+      </TableRow>
+   )
+}
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,9 +1,15 @@
 import { Helmet } from "react-helmet-async";
 
-import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
-import { ArrowRight, Search, X } from "lucide-react";
+import {
+   Table,
+   TableBody,
+   TableHead,
+   TableHeader,
+   TableRow
+} from "@/components/ui/table";
+
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
 
 export function Orders() {
    return (
@@ -17,13 +23,7 @@ export function Orders() {
          </div>
 
          <div className="space-y-2.5">
-            <form className="flex items-center gap-2">
-               <span className="text-sm font-semibold">
-                  Filtros
-               </span>
-
-               <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-            </form>
+            <OrderTableFilters />
 
             <div className="border rounded-md">
                <Table>
@@ -43,51 +43,7 @@ export function Orders() {
                   <TableBody>
                      {Array.from({ length: 10 }).map((_, index) => {
                         return (
-                           <TableRow key={index}>
-                              <TableCell>
-                                 <Button variant={"outline"} size={"xs"}>
-                                    <Search className="max-h-3 max-w-3" />
-                                    <span className="sr-only">Detalhes do pedido</span>
-                                 </Button>
-                              </TableCell>
-
-                              <TableCell className="font-mono text-xs font-medium">
-                                 395t347596238
-                              </TableCell>
-
-                              <TableCell className="text-muted-foreground">
-                                 Há 15 minutos
-                              </TableCell>
-
-                              <TableCell>
-                                 <div className="flex items-center gap-2">
-                                    <span className="h-2 w-2 rounded-full bg-slate-400" />
-                                    <span className="font-medium text-muted-foreground">Pendente</span>
-                                 </div>
-                              </TableCell>
-
-                              <TableCell className="font-medium">
-                                 João Pedro Cruz
-                              </TableCell>
-
-                              <TableCell className="font-medium">
-                                 R$ 149,90
-                              </TableCell>
-
-                              <TableCell>
-                                 <Button variant={"outline"} size={"xs"}>
-                                    <ArrowRight className="h-3 w-3" />
-                                    Aprovar
-                                 </Button>
-                              </TableCell>
-
-                              <TableCell>
-                                 <Button variant={"ghost"} size={"xs"}>
-                                    <X className="h-3 w-3" />
-                                    Cancelar
-                                 </Button>
-                              </TableCell>
-                           </TableRow>
+                           <OrderTableRow key={index} />
                         )
                      })
                      }
