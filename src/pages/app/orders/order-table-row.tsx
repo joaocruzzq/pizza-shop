55a127c03d2f52c38133f7b7d1cb3f4f30d7e350
joão pedro cruz <joaocruzzdev@gmail.com>
2025-05-10 14:24:08 -0300

    componente order details

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -2,15 +2,24 @@ import { Search, ArrowRight, X } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { TableCell, TableRow } from "@/components/ui/table";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
    return (
       <TableRow>
          <TableCell>
-            <Button variant={"outline"} size={"xs"}>
-               <Search className="max-h-3 max-w-3" />
-               <span className="sr-only">Detalhes do pedido</span>
-            </Button>
+            <Dialog>
+               <DialogTrigger asChild>
+                  <Button variant={"outline"} size={"xs"}>
+                     <Search className="max-h-3 max-w-3" />
+                     <span className="sr-only">Detalhes do pedido</span>
+                  </Button>
+               </DialogTrigger>
+
+               <OrderDetails />
+            </Dialog>
          </TableCell>
 
          <TableCell className="font-mono text-xs font-medium">
