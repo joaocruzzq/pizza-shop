9174eae908e8c648a38c73b970fcf96c87ae7ecc
joão pedro cruz <joaocruzzdev@gmail.com>
2025-06-04 14:34:19 -0300

    criando pagina 404 not found

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router";
 
+import { NotFound } from "./pages/404";
+
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 
@@ -13,6 +15,7 @@ export const router = createBrowserRouter([
    {
       path: "/",
       element : <AppLayout />,
+      errorElement: <NotFound />,
 
       children: [
          { path: "/", element: <Dashboard />},
