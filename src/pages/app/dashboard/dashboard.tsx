fd89c65398111a66f888c370967abf0141c5f181
joão pedro cruz <joaocruzzdev@gmail.com>
2025-06-04 14:28:10 -0300

    criando grafico de produtos populares

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,7 +4,9 @@ import { MonthRevenueCard } from "./month-revenue-card"
 import { DayOrdersAmountCard } from "./day-orders-amount-card"
 import { MonthOrdersAmountCard } from "./month-orders-amount-card"
 import { MonthCanceledOrdersAmountCard } from "./month-canceled-orders-amount"
+
 import { RevenueChart } from "./revenue-chart"
+import { PopularProductsChart } from "./popular-products-chart"
 
 export function Dashboard() {
    return (
@@ -28,6 +30,8 @@ export function Dashboard() {
 
             <div className="grid grid-cols-9 gap-4">
                <RevenueChart />
+
+               <PopularProductsChart />
             </div>
          </div>
       </>
